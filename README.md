826bad793a31415b2bae366ec2b814222185b5e1
Felipe Morales <felipemorales.email@gmail.com>
2021-04-18 19:20:54 -0300

    finalizada primeira aula com o Ruis

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # minicurso-cubo-academy
 Repositorio para estudos do curso preparatório teste Ifood
+
+# Passos para criar um novo exercicio:
+1. Copiar o arquivo template.js
+1. Renomear o arquivo copiado (exemplo: super_novo_arquivo.js)
+1. Atualizar no executor.html na linha 4 o endereço da src (exemplo: src="super_novo_arquivo.js")
+1. Abrir no chrome o arquivo executor.html
+1. Trabalhar no novo arquivo e ir depurando (Toda vez q alterar o js atualiza o chrome, se der pau fecha  a pagina e abre novamente)
+1. Aprender tudo mermo !! Bora rapah!!!
--- exercicio1.js
+++ exercicio1.js
@@ -10,30 +10,15 @@
 		console.log('4 - FUDEU');
 	}
 
-	function somaDoisNumeros(entrada1, entrada2) {
+	function calculoDoJiraya(entrada1, entrada2) {
 		let saida = entrada1 + entrada2; 
+		saida = saida * 5;
+		saida = saida / 200;
 		return saida;
 	}
 	
 	let resultados = [];
-
-	for(let index =1; index < 3; index + 1) {
-		let numero1 = 5;
-		let numero2 = 6;
-		resultados[0] = somaDoisNumeros(numero1, numero2);
-	}
-	
-	let numero1 = 5;
-	let numero2 = 6;
-	resultados[0] = somaDoisNumeros(numero1, numero2);
-
-	numero1 = 7;
-	numero2 = 11;
-	resultados[1] = somaDoisNumeros(numero1, numero2);
-
-	numero5 = 14;
-	numero6 = 22;
-	resultados[2] = somaDoisNumeros(numero5, numero6);
-
-	
+	resultados[0] = calculoDoJiraya(5, 6);
+	resultados[1] = calculoDoJiraya(7, 11);
+	resultados[2] = calculoDoJiraya(14, 22);	
 })()
